4cc0a4240cdc9a6bb4394777a37420290748dc92
Oleksandr Prostotin <37674127+prostotin@users.noreply.github.com>
2019-03-27 23:00:28 -0400

    Update healthHUD.lua
    Fixed code

--- healthHUD.lua
+++ healthHUD.lua
@@ -1,5 +1,5 @@
 --code by prost
-localPlayer = entities.getLocalPlayer();
+localPlayer = entities.GetLocalPlayer();
 -- cl_hud_color values
 --   0: Default
 --   1: White
@@ -13,18 +13,16 @@ localPlayer = entities.getLocalPlayer();
 --   9: Aqua
 --   10: Pink
 function healthHud()
-  if(entity ~= nil) then
-  health = localPlayer.GetHealth();
-  if(health > 99) then
-  client.Command("cl_hud_color 8") --   8: Green
-
-elseif(health > 69) then --69 makes your resolver better
-  client.Command("cl_hud_color 7") --   7: Yellow
-
-elseif(health > 49) then
-  client.Command("cl_hud_color 6") --   6: Orange
-else
-  client.Command("cl_hud_color 5") --   5: Red
+  if(localPlayer ~= nil) then
+   health = entities.GetLocalPlayer():GetHealth();
+  if(health > 95) then
+   client.Command("cl_hud_color 8", true) --   8: Green
+  elseif(health > 69) then --69 makes your resolver better
+   client.Command("cl_hud_color 7", true) --   7: Yellow
+  elseif(health > 39) then
+   client.Command("cl_hud_color 6", true) --   6: Orange
+  else
+   client.Command("cl_hud_color 5", true)--   5: Red
 end
 end
 end
