ce2200e12b0f8b15860e6027e594caeabd54845b
Oleksandr Prostotin <37674127+prostotin@users.noreply.github.com>
2019-03-28 02:01:38 -0400

    Update randomSlowwalk.lua
    Fixed code

--- randomSlowwalk.lua
+++ randomSlowwalk.lua
@@ -1,21 +1,24 @@
 -- made by prost
-miscRef = gui.Reference("MISC", "AUTOMATION", "xd");
-min = gui.Slider(miscRef, "min", "Min Speed %", 0, 0, 100);
-max = gui.Slider(miscRef, "min", "Max Speed %", 0, 0, 100);
+miscRef = gui.Reference("MISC", "AUTOMATION", "Movement");
+min = gui.Slider(miscRef, "Min", "Min", 0, 0, 100);
+max = gui.Slider(miscRef, "Max", "Max", 0, 0, 100);
+
+
 function randomSlowwalk()
 local entity = entities.GetLocalPlayer();
 
 if entity ~= nil then
    local alive = entity:IsAlive();
    local health = entity:GetHealth();
-   if (alive == true and health > 0 and min.GetValue() < max.GetValue()) then
-     local minSpeed = math.floor(min.GetValue());
-     local maxSpeed = math.floor(max.GetValue());
+   local minSpeed = math.floor(min:GetValue());
+   local maxSpeed = math.floor(max:GetValue());
 
-     gui.SetValue("misc_automation_slowwalk", math.random(minSpeed, maxSpeed));
-     
+   if (alive == true and health > 0 and minSpeed < maxSpeed) then
+     gui.SetValue("msc_slowwalkspeed", (math.random(minSpeed, maxSpeed)/100), 1);
 
-   end
+ elseif(minSpeed > maxSpeed) then
+   gui.SetValue("Min", maxSpeed-1, 1);
+    end
 end
 end
 callbacks.Register('Draw', 'randomSlowwalk', randomSlowwalk)
